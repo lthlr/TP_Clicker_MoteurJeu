01b12fb1416e14873df702e7402375d6b553213a
Lisa Thullier <lisathullier@gmail.com>
2024-11-13 11:26:17 +0100

    VieMonstre_encours

--- Assets/Scripts/MainGame.cs
+++ Assets/Scripts/MainGame.cs
@@ -1,22 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MainGame : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
             Vector3 world = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            RaycastHit hit = Physics2D.Raycast(world, Vector2.zero);
+            RaycastHit2D hit = Physics2D.Raycast( world, Vector2.zero);
 
             if (hit.collider != null)
             {
--- Assets/Scripts/Monster.cs
+++ Assets/Scripts/Monster.cs
@@ -1,8 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Monster : MonoBehaviour
 {
     public int Life;
+    public TMP_Text Textlife;
+    public Image ImageLife;
+    int _lifeMax;
+
+    void UpdateLife()
+    {
+        Textlife.text = $"{Life}/{_lifeMax}";
+
+        float percent = (float)Life / (float)_lifeMax;
+        ImageLife.fillAmount = percent;
+    }
+
+    private void Awake()
+    {
+        UpdateLife();
+    }
 }
