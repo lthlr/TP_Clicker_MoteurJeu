59533738737d1515dcb724bd2b4ab63350b7cc1d
Lisa Thullier <lisathullier@gmail.com>
2025-01-26 17:52:15 +0100

    cooking

--- Assets/Scripts/MainGame.cs
+++ Assets/Scripts/MainGame.cs
@@ -1,21 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
+using DG.Tweening;
+using TMPro;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class MainGame : MonoBehaviour
 {
+    public List<MonsterInfos> Monsters;
+    int _currentMonster = 0;
+    public Monster monster;
+    public GameObject PrefabHitPoint;
+
+    private void Start()
+    {
+        monster.SetMonster(Monsters[_currentMonster]);
+    }
+
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
             Vector3 world = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            RaycastHit2D hit = Physics2D.Raycast( world, Vector2.zero);
+            RaycastHit2D hit = Physics2D.Raycast(world, Vector2.zero);
 
             if (hit.collider != null)
             {
-                Debug.Log(hit.collider.name);
+                Monster monster = hit.collider.GetComponent<Monster>();
+                monster.Hit(1);
+
+                GameObject go = GameObject.Instantiate(PrefabHitPoint, monster.canvas.transform, false);
+                go.transform.localPosition = UnityEngine.Random.insideUnitCircle * 5;
+                go.transform.DOLocalMoveY(150, 0.8f);
+                go.GetComponent<TMP_Text>().DOFade(0, 0.8f);
+                GameObject.Destroy(go, 0.8f);
+                
+
+            }
+
+            if (monster.IsAlive() == false)
+            {
+                NextMonster();
             }
         }
+    }
+
+    private void NextMonster()
+    {
+        _currentMonster++;
+        monster.SetMonster(Monsters[_currentMonster]);
     }
 }
--- Assets/Scripts/Monster.cs
+++ Assets/Scripts/Monster.cs
@@ -1,21 +1,52 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
+[Serializable]
+public class MonsterInfos
+{
+    public string Name;
+    public int Life;
+    public Sprite Sprite;
+}
 public class Monster : MonoBehaviour
 {
-    public int Life;
+    [SerializeField]int _life;
     public TMP_Text Textlife;
     public Image ImageLife;
-    int _lifeMax;
+    int _lifeMax = 10;
+    public GameObject Visual;
+    public Canvas canvas;
 
+    public bool IsAlive()
+    {
+        return _life > 0;
+    }
+
+    public void SetMonster(MonsterInfos infos)
+    {
+        _lifeMax = infos.Life;
+        _life = _lifeMax;
+
+        Visual.GetComponent<SpriteRenderer>().sprite = infos.Sprite;
+    }
+    public void Hit(int damage)
+    {
+        _life -= damage;
+        UpdateLife();
+        Visual.transform.DOComplete();
+        Visual.transform.DOPunchScale(new Vector3(0.2f, 0.2f, 0), 0.3f);
+    }
     void UpdateLife()
     {
-        Textlife.text = $"{Life}/{_lifeMax}";
+        Textlife.text = $"{_life}/{_lifeMax}";
 
-        float percent = (float)Life / (float)_lifeMax;
+        float percent = (float)_life / (float)_lifeMax;
         ImageLife.fillAmount = percent;
     }
 
@@ -24,3 +55,5 @@ public class Monster : MonoBehaviour
         UpdateLife();
     }
 }
+
+
